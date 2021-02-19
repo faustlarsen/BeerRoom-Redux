681ac947ec6bb19a6d252259c0477338903952ed
Constantine <faustlarsen@gmail.com>
2021-02-19 10:10:19 -0700

    pass test to add beer

--- src/reducers/beer-list-reducer.js
+++ src/reducers/beer-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, alcoholContent, price, id } = action;
+  switch (action.type) {
+  case 'ADD_BEER':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        alcoholContent: alcoholContent,
+        price: price,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
