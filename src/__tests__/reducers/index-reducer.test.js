b00ea8d94f1b9786549e59a296574ad2139ea17f
Constantine <faustlarsen@gmail.com>
2021-02-19 11:56:05 -0700

    pass test to return default state of each reducer

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,25 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import beerListReducer from '../../reducers/beer-list-reducer';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      masterBeerList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+  test('Check that initial state of beerListReducer matches root reducer', () => {
+    expect(store.getState().masterBeerList).toEqual(beerListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+});
